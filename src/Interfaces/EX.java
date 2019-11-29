10cd262c006c3f5ea3adc0906d317200d1f31633
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-29 14:18:05 +0100

    Interfaces og polymophism

---
+++ src/Interfaces/EX.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface EX {
+    int sej = 0;
+
+    public void ex();
+}
--- src/Interfaces/ElectricPokemon.java
+++ src/Interfaces/ElectricPokemon.java
@@ -1,5 +1,12 @@
 package Interfaces;
 
-public class ElectricPokemon extends Pokemon {
-    public void givStoed(){}
+/** Eksempel på multinedarvning
+ * I java kan en klasse nedarve fra uendelig mange interfaces (polymorfi),
+ * men kun fra én superclass. */
+public class ElectricPokemon extends Pokemon implements EX{
+
+    @Override
+    public void ex() {
+        System.out.println("Jeg er ekstra sej.");
+    }
 }
